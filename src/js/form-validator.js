c674c7e74547ca848d06e211cd1579669e276c87
Bruno Castro <bs8872491@gmail.com>
2022-10-26 07:54:31 -0300

    Adicionando validador de email

--- src/js/form-validator.js
+++ src/js/form-validator.js
@@ -4,7 +4,7 @@ const btnForm = document.getElementById('button-form');
 btnForm.addEventListener('click', (event) => {
     event.preventDefault();
     const formObj = convertFormToObj(form);
-    if(nameAndLastNameValidator(formObj) && notEmptyForm(formObj)){
+    if(nameAndLastNameValidator(formObj) && notEmptyForm(formObj) && emailValidator(formObj)){
         console.log('Vai dar é certo pvt')
     }
 });
@@ -41,12 +41,23 @@ function notEmptyForm(formObj){
     let result = true;
 
     listValues.forEach( value => {
-        console.log(value)
         if(value !== ''){
             result = true
         }else {
             result = false
         }
     });
+    return result;
+}
+
+function emailValidator(formObj){
+    let result = false;
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const emailStr = formObj.email;
+    const emailResult = re.test(emailStr);
+
+    if (emailResult) {
+        result = true;
+    }
     return result;
 }
