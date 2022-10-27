107c4571a54ba7909f237e337254e26b241f4c98
Bruno Castro <bs8872491@gmail.com>
2022-10-27 15:39:50 -0300

    Fixando a barra de navegação de header apenas em páginas de altura apropriada

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const { scrollTop, scrollHeight } = document.documentElement;
 
-    if(scrollTop > 120){
+    if(scrollTop > 120 && scrollHeight > 1000){
         navbar.classList.add('fixed', 'py-0')
     } else {
         navbar.classList.remove('fixed', 'py-0')
