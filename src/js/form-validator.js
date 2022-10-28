1351a585dc50fb9b74002e775f6aa5388d41e2bb
Bruno Castro <bs8872491@gmail.com>
2022-10-28 14:04:41 -0300

    Otimizando a função de validação do formulário

--- src/js/form-validator.js
+++ src/js/form-validator.js
@@ -3,8 +3,7 @@ const btnForm = document.getElementById('button-form');
 
 btnForm.addEventListener('click', (event) => {
     event.preventDefault();
-    const formObj = convertFormToObj(form);
-    if(formValidator(formObj)){
+    if(formValidator(form)){
         console.log('Vai dar é certo pvt')
     }
 });
@@ -19,66 +18,38 @@ function convertFormToObj(form) {
     return form;
 }
 
-function formValidator(formObj){
-    let result = false;
-    if(nameAndLastNameValidator(formObj) && notEmptyForm(formObj) && emailValidator(formObj) && passwordValidator(formObj)){
-        result = true
-    }
-    return result
-}
-
-function nameAndLastNameValidator(formObj) {
-    let result = false;
-
-    const reNumber = /\d/;
-    const nameStr = formObj.name;
-    const lastNameStr = formObj.lastName;
-    const nameResult = reNumber.test(nameStr);
-    const lastNameResult = reNumber.test(lastNameStr);
-    
-    if(nameResult || lastNameResult){
-        result = false
-    } else {
-        result = true
-    }
-    return result
-}
-
-function notEmptyForm(formObj){
-    const listValues = Object.values(formObj);
-    let result = true;
-
-    listValues.forEach( value => {
-        if(value !== ''){
-            result = true
-        }else {
-            result = false
-        }
-    });
+function formValidator(form){
+    const formObj = convertFormToObj(form);
+    let result = (nameAndLastNameValidator(formObj) && emailValidator(formObj) && passwordValidator(formObj)) ? true : false;
     return result;
 }
 
-function emailValidator(formObj){
-    let result = false;
-    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    const emailStr = formObj.email;
-    const emailResult = re.test(emailStr);
+function nameAndLastNameValidator(formObj) {
+    const regexNameAndLastName = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]{2,}$/;
+    const nameStr = formObj.name;
+    const lastNameStr = formObj.lastName;
+    const nameResult = regexNameAndLastName.test(nameStr);
+    const lastNameResult = regexNameAndLastName.test(lastNameStr);
+    
+    let result = (nameResult && lastNameResult) ? true : false;
+    return result
+}
 
-    if (emailResult) {
-        result = true;
-    }
+function emailValidator(formObj){
+    const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const emailStr = formObj.email;
+    const emailResult = regexEmail.test(emailStr);
+
+    let result = emailResult ? true : false;
     return result;
 }
 
 function passwordValidator(formObj){
-    const re = /(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g;
+    const regexStrongPassword = /(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g;
     const passStr = formObj.password;
-    let result = false;
-    const passResult = re.test(passStr);
+    const passResult = regexStrongPassword.test(passStr);
 
-    if (passResult) {
-        result = true
-    }
+    let result = passResult ? true : false;
     return result;
 
 }
